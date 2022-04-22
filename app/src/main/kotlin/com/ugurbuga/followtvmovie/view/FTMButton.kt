c9a7bc88f861f0718f158de7dfd4d54437f446ec
Uğur Buğa <47065142+UgurBuga@users.noreply.github.com>
2022-04-22 23:01:03 +0300

    add ftmbutton
    add ftmbutton

---
+++ app/src/main/kotlin/com/ugurbuga/followtvmovie/view/FTMButton.kt
@@ -0,0 +1,14 @@
+package com.ugurbuga.followtvmovie.view
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.button.MaterialButton
+import com.ugurbuga.followtvmovie.R
+
+class FTMButton @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleAttr: Int = R.attr.ftmButton
+) : MaterialButton(
+    context, attributeSet, defStyleAttr
+)
