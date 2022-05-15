3fbcc7d2eaf1b70af2f999ef997a77c39a52a4ad
Uğur Buğa <47065142+UgurBuga@users.noreply.github.com>
2022-05-15 12:37:05 +0300

    fix webview dark mode
    fix webview dark mode

---
+++ app/src/main/kotlin/com/ugurbuga/followtvmovie/view/FTMUiModeCareWebView.kt
@@ -0,0 +1,17 @@
+package com.ugurbuga.followtvmovie.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.webkit.WebView
+import com.ugurbuga.followtvmovie.extensions.fixUiModeIfNeeded
+
+open class FTMUiModeCareWebView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : WebView(context, attrs, defStyleAttr) {
+
+    init {
+        context.fixUiModeIfNeeded()
+    }
+}
